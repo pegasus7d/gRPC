4506ebce15a95b7bf0f486dd4e7fcbd73e6bf672
Debayan Biswas <debayanbiswas1111@gmail.com>
2023-09-22 22:20:05 +0530

    done writing the boiler plate of client and server and running them using their exe files

---
+++ greet/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/pegasus7d/grpc/greet/proto"
+	"google.golang.org/grpc"
+)
+
+var addr string = "localhost:50051"
+
+type Server struct{
+	pb.GreetServiceServer
+}
+
+func main() {
+
+	lis,err := net.Listen("tcp",addr)
+	if err!=nil{
+		log.Fatalf("Failed to listen on:%v\n",err)
+	}
+
+	log.Printf("Listening on %s\n",addr)
+	s:=grpc.NewServer()
+
+	if err=s.Serve(lis);err!=nil{
+		log.Fatalf("Failed to serve on:%v\n",err)
+	}
+}
