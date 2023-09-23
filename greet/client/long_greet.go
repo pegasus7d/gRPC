139cdb8b08490774002f346c1fa4f5945aee2ebc
Debayan Biswas <debayanbiswas1111@gmail.com>
2023-09-23 14:33:47 +0530

    Client streaming api done

---
+++ greet/client/long_greet.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/pegasus7d/grpc/greet/proto"
+)
+
+func doLongGreet(c pb.GreetServiceClient){
+	log.Println("doLongGreet was invoked")
+	reqs:=[]*pb.GreetRequest{
+		{FirstName: "Debayan"},
+		{FirstName: "Eshanika"},
+		{FirstName: "Debayan"},
+		{FirstName: "Eshanika"},
+	}
+	stream,err:=c.LongGreet(context.Background())
+	if err != nil {
+		log.Fatalf("Error while calling LongGreet: %v\n", err)
+	}
+
+	for _,req:= range reqs{
+		log.Printf("Sending req:%v\n",req)
+		stream.Send(req)
+		time.Sleep(1*time.Second)
+	}
+
+	res,err:=stream.CloseAndRecv()
+
+	if err!=nil{
+		log.Fatalf("Error while receiving response%v\n",err)
+	}
+
+	log.Printf("LongGreet %s",res.Result)
+
+}
